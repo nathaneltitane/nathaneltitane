d5f0a00e3c6f17b4c0b6d39c111206bc8742ca61
Nathanel Titane <nathanel.titane@gmail.com>
2020-03-04 15:14:21 -0500

    JS update

--- javascript/set-random-logo.js
+++ javascript/set-random-logo.js
@@ -3,14 +3,14 @@
 $(document).ready(function() {
 
 	var logo_array = [
-						['logo-01'],
-						['logo-02'],
-						['logo-03'],
-						['logo-04'],
-						['logo-05']
+						['logo-blue'],
+						['logo-green'],
+						['logo-purple'],
+						['logo-red'],
+						['logo-teal']
 						];
 
 	var index = Math.floor(Math.random() * logo_array.length);
 
-	$('.logo').css('background-image', 'url("/logo/' + logo_array[index] + '.svg")');
+	$('.logo').css('background-image', 'url("/assets/' + logo_array[index] + '.svg")');
 });
