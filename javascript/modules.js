cb3c2f2d480f36e5d232f9ca6fef470505525fb0
nathaneltitane <nathanel.titane@gmail.com>
2023-10-18 19:50:13 -0400

    Code & Features Update

--- javascript/modules.js
+++ javascript/modules.js
@@ -1,28 +1,28 @@
 // modules //
 
-$(function() {
+$( function ( ) {
 
-	$.get("/modules/head.html", function(data) {
+	$.get ( '/modules/head.html', function ( data ) {
 
-		$("head").append(data);
-
-	} );
-
-	$.get( "/modules/drawer.html", function ( data ) {
-
-		$( "#drawer" ).append ( data );
+		$('head' ).append ( data );
 
 	} );
 
-	$.get( "/modules/navigation.html", function ( data ) {
+	$.get( '/modules/drawer.html', function ( data ) {
 
-		$( "#navigation" ).append ( data );
+		$( '#drawer' ).append ( data );
 
 	} );
 
-		$.get( "/modules/load.html", function ( data ) {
+	$.get( '/modules/navigation.html', function ( data ) {
 
-		$( "#load" ).append ( data );
+		$( '#navigation' ).append ( data );
+
+	} );
+
+		$.get( '/modules/load.html', function ( data ) {
+
+		$( '#load' ).append ( data );
 
 	} );
 
